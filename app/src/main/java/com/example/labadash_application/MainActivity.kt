744ea4511c7b92f06d24559e0020124e9bf30a39
semion-jk <jks8210@students.uc-bcf.edu.ph>
2024-10-11 17:26:52 +0800

    Revert "pull req"

--- app/src/main/java/com/example/labadash_application/MainActivity.kt
+++ app/src/main/java/com/example/labadash_application/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.example.labadash_application
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,13 +9,12 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-
-        supportActionBar?.hide()
-
-        Handler().postDelayed({
-            val intent = Intent(this@MainActivity, Home::class.java)
-            startActivity(intent)
-        }, 3000)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
         }
     }
+}
