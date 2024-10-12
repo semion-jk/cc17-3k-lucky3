5f27082fd3afb0327a3a8e9be2e2012e679490c6
John Lucky Louis Pilit <j-p7387@students.uc-bcf.edu.ph>
2024-10-13 06:02:32 +0800

    Adding Welcome Page

---
+++ app/src/main/java/com/example/labadash_application/welcomeActivity.kt
@@ -0,0 +1,21 @@
+package com.example.labadash_application
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class welcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.welcome)
+
+        supportActionBar?.hide()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, welcomeActivity::class.java)
+            startActivity(intent)
+        }, 2000)
+    }
+}
