21374c21d0d93f385571f107ccac30f4df8b3998
John Lucky Louis Pilit <j-p7387@students.uc-bcf.edu.ph>
2024-10-13 06:08:20 +0800

    Adding files and Updating file names

--- app/src/main/java/com/example/labadash_application/LoginActivity.kt
+++ app/src/main/java/com/example/labadash_application/LoginActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
-class Home : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
