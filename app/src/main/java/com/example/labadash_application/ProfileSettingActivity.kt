8c3f00e127dae75230b9c36ff496b31255d5b7ed
John Lucky Louis Pilit <j-p7387@students.uc-bcf.edu.ph>
2024-10-13 21:08:46 +0800

    Update in Log out button

--- app/src/main/java/com/example/labadash_application/ProfileSettingActivity.kt
+++ app/src/main/java/com/example/labadash_application/ProfileSettingActivity.kt
@@ -5,12 +5,20 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.FrameLayout
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class ProfileSettingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.profile_setting_screen)
+
+        val logout = findViewById<TextView>(R.id.logout)
+
+        logout.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
 
         val contentFrame = findViewById<FrameLayout>(R.id.content_frame)
 
