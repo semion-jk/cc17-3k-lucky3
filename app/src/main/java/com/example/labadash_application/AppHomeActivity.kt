39716d7a4fb053a464e3ff0a643a479254e1799f
John Lucky Louis Pilit <j-p7387@students.uc-bcf.edu.ph>
2024-10-13 10:04:06 +0800

    Adding File for Home Page

---
+++ app/src/main/java/com/example/labadash_application/AppHomeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.labadash_application
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AppHomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.app_home_page)
+
+    }
+}
